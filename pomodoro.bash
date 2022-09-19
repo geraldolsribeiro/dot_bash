f882318e95abb9283cc9cce25471be4e6b169c3f
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2022-09-19 10:23:10 -0300

    By hostname db; Edit function

--- pomodoro.bash
+++ pomodoro.bash
@@ -1,51 +1,84 @@
 
-# Show time elapsed for last open pomodoro (timestamp 2 equal to -1)
+pomodoro_database() {
+  echo "$HOME/.bash/pomodoro.$HOSTNAME.db"
+}
+
+# Show time elapsed for last open pomodoro (second timestamp is equal to -1)
 pomodoro_time() {
-  local database="$HOME/.bash/pomodoro.db"
   local task
   local timestamp1
   local timestamp2
   local differenceInSeconds
-  local ret=""
-  if [ -f "$database" ];then
-    timestamp1=$(tail -1 ~/.bash/pomodoro.db | cut -f 3)
-    timestamp2=$(tail -1 ~/.bash/pomodoro.db | cut -f 4)
-    task=$(tail -1 ~/.bash/pomodoro.db | cut -f 5)
-    if [ "$timestamp2" = "-1" ]; then
-      timestamp2=$(date +%s)
-      differenceInSeconds=$((timestamp2-timestamp1))
 
-      if [ $differenceInSeconds -gt $(( 25*60 )) ]; then
-        notify-send -u critical "Time to rest!"
-      fi
-
-      ret=$(date "+ 🍅%M:%S $task" --utc --date "@$differenceInSeconds")
-    fi
+  if [ ! -f "$(pomodoro_database)" ]; then
+    return
   fi
-  echo -n "$ret"
+
+  timestamp1=$(tail -1 "$(pomodoro_database)" | cut -f 3)
+  timestamp2=$(tail -1 "$(pomodoro_database)" | cut -f 4)
+  task=$(tail -1 "$(pomodoro_database)" | cut -f 5)
+
+  # ignore if it is finished
+  if [ "$timestamp2" != "-1" ]; then
+    return
+  fi
+
+  timestamp2=$(date +%s)
+  differenceInSeconds=$((timestamp2-timestamp1))
+
+  # Notify +25m
+  if [ $differenceInSeconds -gt $(( 25*60 )) ]; then
+    notify-send -u critical "Time to rest!"
+  fi
+
+  # Print the elapsed time and the task name
+  date "+ 🍅%M:%S $task" --utc --date "@$differenceInSeconds"
 }
 
 pomodoro_start() {
-  local database="$HOME/.bash/pomodoro.db"
   local task=${1:-Urgent}
   local timestamp1="-1"
   local timestamp2="-1"
   local elapsedMinutes="-1"
-  local n
-  n=$(wc -l < "$database")
+  local n="0"
+
+  if [ -f "$(pomodoro_database)" ]; then
+    n=$(wc -l < "$(pomodoro_database)")
+  fi
+
+  if [ "$n" != "0" ]; then
+    pomodoro_finish
+  fi
+
   timestamp1=$(date +%s)
-  echo -e "${n}\t${elapsedMinutes}\t${timestamp1}\t${timestamp2}\t${task}" >> "$database" 
+  echo -e "${n}\t${elapsedMinutes}\t${timestamp1}\t${timestamp2}\t${task}" >> "$(pomodoro_database)" 
 }
 
 pomodoro_finish() {
-  local database="$HOME/.bash/pomodoro.db"
   local timestamp1
   local timestamp2
   local minutes
-  timestamp1=$(tail -1 ~/.bash/pomodoro.db | cut -f 3)
+
+  if [ ! -f "$(pomodoro_database)" ]; then
+    return
+  fi
+
+  timestamp1=$(tail -1 "$(pomodoro_database)" | cut -f 3)
+  timestamp2=$(tail -1 "$(pomodoro_database)" | cut -f 4)
+
+  # ignore if it is finished
+  if [ "$timestamp2" != "-1" ]; then
+    return
+  fi
+
   timestamp2=$(date +%s)
   minutes=$(( (timestamp2-timestamp1)/60 ))
-  sed -i "$ s/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/\1\t${minutes}\t\3\t${timestamp2}\t\5/" "$database"
+  # Edit the last line
+  sed -i "$ s/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/\1\t${minutes}\t\3\t${timestamp2}\t\5/" "$(pomodoro_database)"
+}
+
+pomodoro_edit() {
+  $EDITOR "$(pomodoro_database)"
 }
 
 # vim: ft=bash
